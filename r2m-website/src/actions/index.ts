428609727346df7113d09af49d7ecb49fcbfb023
campos <nonesovile96@hotmail.com>
2025-04-14 21:23:41 -0300

    Handle registration properly on the website

--- r2m-website/src/actions/index.ts
+++ r2m-website/src/actions/index.ts
@@ -1,4 +1,4 @@
-import { defineAction } from 'astro:actions';
+import { defineAction, ActionError } from 'astro:actions';
 import { z } from 'astro:schema';
 
 export const server = {
@@ -11,16 +11,32 @@ export const server = {
       code: z.string(),
     }),
     handler: async ({ email, password, password_confirmation, code }) => {
-        const backendName = import.meta.env.BACKEND_NAME;
-        await fetch(`https://${backendName}/_r2m/register`, {
-            method: "POST",
-            body: JSON.stringify({
-                email: email,
-                password: password,
-                password_confirmation: password_confirmation,
-                code: code
-            }),
-        });
+      const backendName = import.meta.env.BACKEND_NAME;
+      const response = await fetch(`https://${backendName}/_r2m/register`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          password_confirmation: password_confirmation,
+          code: code
+        }),
+      });
+
+      switch (response.status) {
+        case 400:
+          throw new ActionError({
+            code: "BAD_REQUEST",
+            message: await response.json()
+          });
+        case 401:
+          throw new ActionError({
+            code: "UNAUTHORIZED",
+            message: await response.json()
+          });
+      }
     },
   })
 }
