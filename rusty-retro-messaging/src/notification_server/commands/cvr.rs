9d4b88ea22cc4549e285c9e2a40199eded4886eb
campos02 <nonesovile96@hotmail.com>
2025-08-25 00:01:37 -0300

    Lower version in CVR

--- rusty-retro-messaging/src/notification_server/commands/cvr.rs
+++ rusty-retro-messaging/src/notification_server/commands/cvr.rs
@@ -15,7 +15,7 @@ impl Command for Cvr {
         let tr_id = *args.get(1).ok_or(CommandError::NoTrId)?;
 
         Ok(vec![format!(
-            "CVR {tr_id} 1.0.0000 1.0.0000 7.0.0425 https://r2m.camposs.net/storage https://r2m.camposs.net\r\n"
+            "CVR {tr_id} 1.0.0000 1.0.0000 1.0.0000 https://r2m.camposs.net/storage https://r2m.camposs.net\r\n"
         )])
     }
 }
