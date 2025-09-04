e85b938002d3fbf906e512807b4e48c1adc4d731
campos02 <nonesovile96@hotmail.com>
2025-09-04 02:00:50 -0300

    Rename SERVER_NAME to SERVER_DOMAIN

--- src/http/nexus.rs
+++ src/http/nexus.rs
@@ -2,6 +2,6 @@ use axum::response::IntoResponse;
 use std::env;
 
 pub async fn nexus() -> impl IntoResponse {
-    let server_name = env::var("SERVER_NAME").expect("SERVER_NAME not set");
+    let server_name = env::var("SERVER_DOMAIN").expect("SERVER_DOMAIN not set");
     [("PassportURLs", format!("DALogin={server_name}/login.srf"))]
 }
--- src/notification_server/commands/url.rs
+++ src/notification_server/commands/url.rs
@@ -15,7 +15,7 @@ impl Command for Url {
         let args: Vec<&str> = command.trim().split(' ').collect();
         let tr_id = *args.get(1).ok_or(CommandError::NoTrId)?;
         let server_name =
-            env::var("SERVER_NAME").or(Err(CommandError::Reply(format!("500 {tr_id}\r\n"))))?;
+            env::var("SERVER_DOMAIN").or(Err(CommandError::Reply(format!("500 {tr_id}\r\n"))))?;
 
         Ok(vec![format!(
             "URL {tr_id} /url https://{server_name}/url 1\r\n"
